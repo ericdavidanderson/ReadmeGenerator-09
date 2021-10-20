e1bd16a9cca20949a718ce9feb15ac2c79bbd418
ericdavidanderson <burrej1@gmail.com>
2021-10-20 12:07:13 -0700

    index.js file

---
+++ index.js
@@ -0,0 +1,29 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    //Enter a description of the Readme you wish to generate:
+
+    //Instalation Instructions:
+
+    //Usage:
+
+    //Enter Project Title:
+
+    //Badge for license and notice of license here:
+
+    //What is your Github username?
+
+    //What is your email?
+
+    
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
