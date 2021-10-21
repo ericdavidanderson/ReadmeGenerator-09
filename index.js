7a5fc12abd4b99e0f1df191266dc6f78325d6872
ericdavidanderson <burrej1@gmail.com>
2021-10-20 23:02:05 -0700

    update with basic structure

--- index.js
+++ index.js
@@ -1,29 +1,74 @@
 // TODO: Include packages needed for this application
-
+var inquirer = require("inquirer");
+var fs = require("fs");
+var generateMarkdown = require("./generateMarkdown");
+// inquirer
+//   .prompt([
+//     /* Pass your questions in here */
+//   ])
+//   .then((answers) => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
 // TODO: Create an array of questions for user input
 const questions = [
-    //Enter a description of the Readme you wish to generate:
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of the Readme you wish to generate:",
+  },
 
-    //Instalation Instructions:
+  //Instalation Instructions:
 
-    //Usage:
+  //Usage:
 
-    //Enter Project Title:
+  { type: "input", name: "title", message: "Enter Project Title:" },
 
-    //Badge for license and notice of license here:
+  {
+    type: "list",
+    name: "license",
+    message: "Please choose the badge for license and notice of license here:",
+    choices: ["mit", "none"],
+  },
 
-    //What is your Github username?
+  //What is your Github username?
 
-    //What is your email?
-
-    
+  //What is your email?
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+      /* Pass your questions in here */
+    
+    .then((answers) => {
+      writeToFile("./dist/Readme.md", generateMarkdown(answers))  
+      // Use user feedback for... whatever!!
+      console.log(answers)
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
 
 // Function call to initialize app
 init();
